60c8a2cf15eedb7ec6c6327813c06307187b2ec2
Andrew Glover <amglover256@gmail.com>
2022-06-23 21:38:11 -0400

    Optimize parameters, better UI.

--- app.R
+++ app.R
@@ -1,12 +1,15 @@
 library(shiny)
 library(ggplot2)
 
+
 #cute functional programing stuff
-inputPar=function(id){
+params_tmp <- c("p_0","k_1","k_2","tau_1","tau_2")
+
+inputPar <- function(id){
   textInput(id, label = paste("Choose",id,sep=" "), value = NULL, placeholder ="number")
 }
-params_tmp=c("p_0","k_1","k_2","tau_1","tau_2")
-inputParams=purrr::map(params_tmp,inputPar)
+
+inputParams <- purrr::map(params_tmp,inputPar)
 
 
 vars <- tibble::tribble(
@@ -16,7 +19,7 @@ vars <- tibble::tribble(
   "performance",    "Select Performance"
 )
 
-myInput=function(id,label){
+myInput <- function(id,label){
   selectInput(id,label,choices=NULL)
 }
 inputsData <- purrr::pmap(vars, myInput)
@@ -25,7 +28,7 @@ inputsData <- purrr::pmap(vars, myInput)
 
 #model functions
 #clean up parameters to function
-Predicted.Performance=function(params,Training.Load,day=length(Training.Load)){
+predictedPerformance <- function(params,Training.Load,day=length(Training.Load)){
   p_0=params[1]; k_1=params[2]; k_2=params[3]; tau_1=params[4]; tau_2=params[5]
   out=c(); sum_1=0; sum_2=0
   for(t in 1:(day)){
@@ -39,8 +42,8 @@ Predicted.Performance=function(params,Training.Load,day=length(Training.Load)){
   return(out)
 }
 
-SSE=function(params,Training.Load,Performance,day=length(Training.Load)){
-  Pred=Predicted.Performance(params,Training.Load,day)
+SSE <- function(params,Training.Load,Performance,day=length(Training.Load)){
+  Pred=predictedPerformance(params,Training.Load,day)
   #Performance=Performance[1:day-1]
   error=Performance[!is.na(Performance)] - Pred[which(!is.na(Performance))]
   error=error[!is.na(error)]
@@ -48,7 +51,7 @@ SSE=function(params,Training.Load,Performance,day=length(Training.Load)){
   return(SSE)
 }
 
-optim_par=function(v,Training.Load,Performance,day=length(Performance)){
+optim_par <- function(v,Training.Load,Performance,day=length(Performance)){
   x=optim(par = v,  
           fn = SSE, Training.Load = Training.Load, 
           Performance = Performance,
@@ -57,28 +60,13 @@ optim_par=function(v,Training.Load,Performance,day=length(Performance)){
 }
 
 
-
-
-fluidPage(
-  tabsetPanel(
-    tabPanel("Import data", 
-             fileInput("file", "Data", buttonLabel = "Upload..."),
-             textInput("delim", "Delimiter (leave blank to guess)", ""),
-             numericInput("skip", "Rows to skip", 0, min = 0),
-             numericInput("rows", "Rows to preview", 10, min = 1)
-    ),
-    tabPanel("Set parameters"),
-    tabPanel("Visualise results")
-  )
-)
-
 #appUI
 ui=fluidPage(
   tabsetPanel(
     id="wizard",
     type="hidden",
     
-    #choosing dataset 
+    #choosing dataset...
     tabPanel("page_1",
       column(3,
         fileInput("file", "Choose CSV file:",
@@ -86,32 +74,33 @@ ui=fluidPage(
                   accept = c("text/csv",
                              "text/comma-separated-values,text/plain",
                              ".csv")),
-        actionButton("page_12", "Set Inputs"),
-      )
+    #...which lets you go to the next page when one is selected    
+        uiOutput("nextPage"),
+      ),
+      column(9,tableOutput("head"))
     ),
     
-    #Choosing Data and parameters
+    #Choosing inputs and graphing
     tabPanel("page_2",
       column(3,
              actionButton("page_21", "Change Data"),
              inputsData
-      ),
+             ),
       
       column(2,
-        actionButton("graph", "Graph!!"),
-        inputParams
-      ),
-      
-      #Plot
-      column(7,plotOutput("plot"))
+             actionButton("optim", "Optimize parameters"),
+             inputParams
+             ),
+    
+      column(7,
+             actionButton("graph", "Graph!!"),
+             plotOutput("plot")
+             )
     ),
       
       
-    #plotting model
-        
-      
       #to see what's up
-      # tableOutput("Preview"),
+      #tableOutput("Preview"),
       # verbatimTextOutput("out")
     
   )
@@ -121,6 +110,11 @@ ui=fluidPage(
 #appServer
 server=function(input,output,session){
   
+  output$nextPage <- renderUI({
+    req(input$file)
+    actionButton("page_12", "Change Inputs")
+  })
+  
   switch_page <- function(i) {
     updateTabsetPanel(inputId = "wizard", selected = paste0("page_", i))
   }
@@ -128,43 +122,69 @@ server=function(input,output,session){
   observeEvent(input$page_12, switch_page(2))
   observeEvent(input$page_21, switch_page(1))
   
-  df <- eventReactive(input$page_12,{
+  df <- eventReactive(input$file,{
     req(input$file)
     out=read.csv(input$file$datapath)
-    names=names(out)
-    #clean this up later
-    updateSelectInput(session,"performance", "Select Performance", choices=names)
-    updateSelectInput(session,"trainingLoad","Select Training Load",choices=names)
-    updateSelectInput(session,"days", "Select Day Column", choices=names)
     out
   })
   
-  #output$Preview=renderTable(head(df()))
   
-  parameters=eventReactive(input$graph,{
+  name=reactive(names(df()))
+  
+  observe({
+    updateSelectInput(session,"performance", "Select Performance", choices=name())
+    updateSelectInput(session,"trainingLoad","Select Training Load",choices=name())
+    updateSelectInput(session,"days", "Select Day Variable", choices=name())
+  })
+  
+  
+  output$head=renderTable(head(df()))
+  
+  input_Params <- reactive({
     tmp=c(input$p_0,input$k_1,input$k_2,
           input$tau_1,input$tau_2)
     tmp=purrr::map_dbl(tmp,as.double)
     tmp
   })
   
-  Pred_Perf=reactive({
-    Predicted.Performance(parameters(),df()[[input$trainingLoad]])
-    })
+  pred_perf <- reactive({
+    predictedPerformance(input_Params(),df()[[input$trainingLoad]])
+  })
+  
   #for testing 
   #output$out=renderPrint(Pred_Perf())
   
-  output$plot=renderPlot({
-    ggplot(df(),aes(x=df()[[input$days]],y=Pred_Perf()))+
-      geom_line(aes(y=Pred_Perf(),colour="black"), size=1)+
+  updateInputPar <- function(id,value){
+    updateTextInput(session=session,inputId=id, 
+                    label=paste("Choose",id,sep=" "), 
+                    value=value, 
+                    placeholder="number"
+                    )
+  }
+
+  optimParams <- observeEvent(input$optim,{
+    tmp=optim_par(input_Params(),df()[[input$trainingLoad]],
+                  df()[[input$performance]])
+    purrr::map2(params_tmp,tmp,updateInputPar)
+    
+  })
+  
+  
+  plots <- eventReactive(input$graph, {
+    ggplot(df(),aes(x=df()[[input$days]],y=pred_perf()))+
+      geom_line(aes(y=pred_perf(),colour="black"), size=1)+
       geom_point(aes(y = df()[[input$performance]], color="red"), shape = 1)+
-      scale_color_manual("", values = c("black", "red"), 
+      scale_color_manual("", values = c("black", "red"),
                          labels = c("Predicted Performance",
                                     "Actual Performance"))
   })
+
+  output$plot <- renderPlot({
+    plots()
+  })
 }
- 
-?updateSelectInput
+
+
 shinyApp(ui,server)
 
 
